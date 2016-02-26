d8b89134b892379a4840c01000115dfd4fdbe37f
Ishan <ishanatmuzaffarpur@gmail.com>
2016-02-26 13:51:46 +0530

    libglues.so restructured to make the hotspot working again

--- app/src/main/java/com/ishan1608/panoramagltest/MainActivity.java
+++ app/src/main/java/com/ishan1608/panoramagltest/MainActivity.java
@@ -17,38 +17,38 @@ package com.ishan1608.panoramagltest;
  * limitations under the License.
  */
 
-        import com.panoramagl.PLCubicPanorama;
-        import com.panoramagl.PLCylindricalPanorama;
-        import com.panoramagl.PLICamera;
-        import com.panoramagl.PLIPanorama;
-        import com.panoramagl.PLIView;
-        import com.panoramagl.PLImage;
-        import com.panoramagl.PLSpherical2Panorama;
-        import com.panoramagl.PLSphericalPanorama;
-        import com.panoramagl.PLView;
-        import com.panoramagl.PLViewListener;
-        import com.panoramagl.enumerations.PLCubeFaceOrientation;
-        import com.panoramagl.hotspots.PLHotspot;
-        import com.panoramagl.hotspots.PLIHotspot;
-        import com.panoramagl.ios.structs.CGPoint;
-        import com.panoramagl.loaders.PLILoader;
-        import com.panoramagl.loaders.PLJSONLoader;
-        import com.panoramagl.structs.PLPosition;
-        import com.panoramagl.transitions.PLITransition;
-        import com.panoramagl.transitions.PLTransitionBlend;
-        import com.panoramagl.utils.PLUtils;
+import com.panoramagl.PLCubicPanorama;
+import com.panoramagl.PLCylindricalPanorama;
+import com.panoramagl.PLICamera;
+import com.panoramagl.PLIPanorama;
+import com.panoramagl.PLIView;
+import com.panoramagl.PLImage;
+import com.panoramagl.PLSpherical2Panorama;
+import com.panoramagl.PLSphericalPanorama;
+import com.panoramagl.PLView;
+import com.panoramagl.PLViewListener;
+import com.panoramagl.enumerations.PLCubeFaceOrientation;
+import com.panoramagl.hotspots.PLHotspot;
+import com.panoramagl.hotspots.PLIHotspot;
+import com.panoramagl.ios.structs.CGPoint;
+import com.panoramagl.loaders.PLILoader;
+import com.panoramagl.loaders.PLJSONLoader;
+import com.panoramagl.structs.PLPosition;
+import com.panoramagl.transitions.PLITransition;
+import com.panoramagl.transitions.PLTransitionBlend;
+import com.panoramagl.utils.PLUtils;
 
-        import android.content.Context;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.AdapterView;
-        import android.widget.ArrayAdapter;
-        import android.widget.Spinner;
-        import android.widget.Toast;
-        import android.widget.ZoomControls;
-        import android.widget.AdapterView.OnItemSelectedListener;
-        import android.view.View.OnClickListener;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.ZoomControls;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends PLView
 {
